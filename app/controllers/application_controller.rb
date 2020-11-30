183f7492f729fc430d02db21e7d5670db7ed3d97
victoriamok <aprilvika@gmail.com>
2020-11-30 19:23:33 +0100

    Update controllers, workers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def route_not_found
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
 end
--- app/workers/request_worker.rb
+++ app/workers/request_worker.rb
@@ -2,7 +2,20 @@ class RequestWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perfom(*args)
-
+  
+  def perform(query)
+    begin
+      response = Request.new(query).call.read_body
+      #abort(response)
+      @jobs = ActiveSupport::JSON.decode(response)
+      #Rails.logger.debug "Here's some info: #{response.inspect}"
+      Rails.logger.info(@jobs.to_json)
+    rescue ActiveSupport::JSON.parse_error
+      Rails.logger.warn("Attempted to decode invalid JSON: #{response}")
+    end
   end
 end
+
+# sAdd("jobs:munich:engineer", "serializedJobAd")
+#
+# sMembers("jobs:munich:engineer") -> "serializedJobAd"
